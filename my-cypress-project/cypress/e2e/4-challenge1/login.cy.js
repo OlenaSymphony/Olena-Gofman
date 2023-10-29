3374014f9fac678724aa6d09977dff1494bb61a9
Olena Gofman <olena.gofman@symphony-solutions.com>
2023-10-30 00:45:55 +0200

    added checking status code for transactions

--- my-cypress-project/cypress/e2e/4-challenge1/login.cy.js
+++ my-cypress-project/cypress/e2e/4-challenge1/login.cy.js
@@ -5,10 +5,17 @@ import RealWordAppLoginPage from "../../pages/realWordAppLoginPage"
 const realWordAppLoginPage = new RealWordAppLoginPage()
 
 describe('Real World App Sign in page', () => {
-  it.only('allows a User sign up, log in and onboard', () => {
+  it('allows a User sign up, log in and onboard', () => {
     realWordAppLoginPage.openRealWorldApp()
     realWordAppLoginPage.signUp()
     realWordAppLoginPage.signIn()
     realWordAppLoginPage.onboard()
-   })
+  })
+  it.only('verifies that the network request status for transactions is 200', () => {
+    realWordAppLoginPage.openRealWorldApp()
+    realWordAppLoginPage.signUp()
+    realWordAppLoginPage.signIn()
+    realWordAppLoginPage.onboard()
+    realWordAppLoginPage.doNewTransaction('Kaylin Homenick')
+  })
 })
--- my-cypress-project/cypress/pages/realWordAppLoginPage.js
+++ my-cypress-project/cypress/pages/realWordAppLoginPage.js
@@ -40,4 +40,14 @@ export default class LoginPage {
         cy.getBySel('user-onboarding-dialog-title').should('contain', 'Finished')
         cy.getBySel('user-onboarding-next').click()
     }
+    doNewTransaction(username) {
+        cy.getBySel('nav-top-new-transaction').click()
+        cy.location("pathname").should("eq", "/transaction/new")
+        cy.getBySelLike('user-list-item').contains(`${username}`).click({ force: true })
+        cy.get('#amount').type('5')
+        cy.get('#transaction-create-description-input').type('for testing purpose')
+        cy.intercept('POST', '/transactions').as('transactions')
+        cy.getBySelLike('submit-payment').click()
+        cy.wait('@transactions').its('response.statusCode').should('equal', 200)
+    }
 }
