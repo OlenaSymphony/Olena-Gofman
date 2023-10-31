6a6e6d43936e3ed33a77e21d525f7f87785e6240
Olena Gofman <olena.gofman@symphony-solutions.com>
2023-10-31 14:24:08 +0200

    optimized tests

--- my-cypress-project/cypress/e2e/4-challenge1/myAccount.cy.js
+++ my-cypress-project/cypress/e2e/4-challenge1/myAccount.cy.js
@@ -7,39 +7,30 @@ let realWordAppPage;
 describe('Real World App functionality', () => {
   beforeEach(() => {
     realWordAppPage = new RealWordAppPage()
+    realWordAppPage.navigateToHomePage()
+    realWordAppPage.openMyAccount()
   })
 
   it('verifies that the User is able to open the User Setting page', () => {
-    realWordAppPage.navigateToHomePage()
-    realWordAppPage.openMyAccount()
+    // the test execution is in pre-conditions
   })
 
   it('verifies that first username cannot be blank in User Setting', () => {
-    realWordAppPage.navigateToHomePage()
-    realWordAppPage.openMyAccount()
     realWordAppPage.verifyErrorForEmptyField('firstName', 'Enter a first name')
   })
 
   it('verifies that last username cannot be blank in User Setting', () => {
-    realWordAppPage.navigateToHomePage()
-    realWordAppPage.openMyAccount()
     realWordAppPage.verifyErrorForEmptyField('lastName', 'Enter a last name')
   })
 
   it('verifies that invalid email address is not accepted in User Setting', () => {
-    realWordAppPage.navigateToHomePage()
-    realWordAppPage.openMyAccount()
     realWordAppPage.verifyErrorForInvalidFormat('email', 'Must contain a valid email address')
   })
 
   it('verifies that invalid phone is not accepted in User Setting', () => {
-    realWordAppPage.navigateToHomePage()
-    realWordAppPage.openMyAccount()
     realWordAppPage.verifyErrorForInvalidFormat('phoneNumber', 'Phone number is not valid')
   })
   it('verifies that user information can be updated correctly in User Setting', () => {
-    realWordAppPage.navigateToHomePage()
-    realWordAppPage.openMyAccount()
     realWordAppPage.updateUserInformation('Test', 'User', 'test@test.com', '987654321')
   })
 
